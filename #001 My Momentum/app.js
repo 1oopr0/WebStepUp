bbd86598c6725fce674c9ded4a3753575784fdba
1oopro <1oo.ooooo.pro@gmail.com>
2021-06-16 19:31:26 +0900

    FORM 형식의 특징을 배우기 전, javascript로 구현

--- #001 My Momentum/app.js
+++ #001 My Momentum/app.js
@@ -4,8 +4,13 @@ const loginInput = loginForm.querySelector("input");
 const loginButton = loginForm.querySelector("button");
 
 function onLoginBtnClick() {
-    console.log("hello", loginInput.value);
-    console.log("click!!!");
+    const username = loginInput.value;
+    if(username === "") {
+        alert("Please write your name");
+    } else if (username.length > 15) {
+        alert("Your name is too long.");
+    }
+    console.log(username);
 }
 
 loginButton.addEventListener("click", onLoginBtnClick);
