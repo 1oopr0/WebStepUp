fed5c726a46f112f2d5857a1109df382fd537df2
1oopro <1oo.ooooo.pro@gmail.com>
2021-06-16 19:34:36 +0900

    Form Submit 활용한 유효성 검사

--- #001 My Momentum/app.js
+++ #001 My Momentum/app.js
@@ -5,11 +5,6 @@ const loginButton = loginForm.querySelector("button");
 
 function onLoginBtnClick() {
     const username = loginInput.value;
-    if(username === "") {
-        alert("Please write your name");
-    } else if (username.length > 15) {
-        alert("Your name is too long.");
-    }
     console.log(username);
 }
 
