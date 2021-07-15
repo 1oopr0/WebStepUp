020e12d0e6635ed355d2fa06cc44e14ddde7345c
1oopr0 <1oo.ooooo.pro@gmail.com>
2021-07-15 17:28:04 +0900

    Save image

--- #002 My Paint/app.js
+++ #002 My Paint/app.js
@@ -3,6 +3,7 @@ const ctx = canvas.getContext("2d");
 const colors = document.getElementsByClassName("jsColor");
 const range = document.getElementById("jsRange");
 const mode = document.getElementById("jsMode");
+const saveBT = document.getElementById("jsSave");
 
 const INITIAL_COLOR= "#2c2c2c";
 const CANVAS_SIZE = 700;
@@ -10,6 +11,8 @@ const CANVAS_SIZE = 700;
 canvas.width = CANVAS_SIZE;
 canvas.height = CANVAS_SIZE;
 
+ctx.fillStyle = "white";
+ctx.fillRect(0, 0, canvas.width, canvas.height);
 ctx.strokeStyle = INITIAL_COLOR;
 ctx.fillStyle = INITIAL_COLOR;
 ctx.lineWidth = 2.5;
@@ -68,6 +71,18 @@ function handleCanvasClick(){
     }
 }
 
+function handleCM(event){
+    event.preventDefault();
+}
+
+function handleSaveClick(){
+    const image = canvas.toDataURL("image/jpeg");
+    const link = document.createElement("a");
+    link.href = image;
+    link.download = "PaintJS[Export]";
+    link.click();
+}
+
 // client : 윈도우 전체에서의 마우스 값
 // offset : 특정 범위에서의 마우스 값
 if(canvas){
@@ -76,6 +91,7 @@ if(canvas){
     canvas.addEventListener("mouseup", stopPainting);
     canvas.addEventListener("mouseleave", stopPainting);
     canvas.addEventListener("click", handleCanvasClick);
+    canvas.addEventListener("contextmenu", handleCM);
 }
 
 Array.from(colors).forEach(color => color.addEventListener("click", handleColorClick));
@@ -86,4 +102,8 @@ if(range) {
 
 if(mode){
     mode.addEventListener("click", handleModeClick);
+}
+
+if(saveBT){
+    saveBT.addEventListener("click", handleSaveClick);
 }
