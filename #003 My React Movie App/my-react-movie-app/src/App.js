2df8e07a36900733c79ab3c7d0ebe5299590a658
1oopr0 <1oo.ooooo.pro@gmail.com>
2021-07-20 01:06:39 +0900

    How to dynamically generate a component

--- #003 My React Movie App/my-react-movie-app/src/App.js
+++ #003 My React Movie App/my-react-movie-app/src/App.js
@@ -4,14 +4,20 @@ function Food({fav}){
   return <h1>I like {fav}</h1>
 }
 
+const whoILike = [
+    { name : "Sarah",
+    value : 21},
+    { name : "Dohun",
+    value : 16},
+    { name : "Kumho",
+    value : 31},
+    { name : "JiHye",
+    value : 25},];
+
 function App() {
   return (
     <div>
-      <Food fav="kimchi"/>
-      <Food fav="ramen"/>
-      <Food fav="samgiopsal"/>
-      <Food fav="chukumi"/>
-      Hello~!!
+      {whoILike.map(who => (<Food fav = {who.name } />))}
     </div>
   );
 }
