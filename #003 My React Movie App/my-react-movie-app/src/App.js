77e0a76bc597c00508786116317ad55664036a2a
1oopr0 <1oo.ooooo.pro@gmail.com>
2021-07-22 13:23:39 +0900

    class component and render

--- #003 My React Movie App/my-react-movie-app/src/App.js
+++ #003 My React Movie App/my-react-movie-app/src/App.js
@@ -1,55 +1,25 @@
 import React from 'react';
 import PropTypes from "prop-types";
 
-const whoILike = [
-  { 
-    key : 1,
-    name : "Sarah",
-    value : 21
-  },
-  { 
-    key : 2,
-    name : "Dohun",
-    value : 16},
-  { 
-    key : 3,
-    name : "Kumho",
-    value : 31
-  },
-  { 
-    key : 4,
-    name : "JiHye",
-    value : 25
-  },];
-
-function Food({fav, age}){
-  return (
-  <div>
-    <h2>I like {fav}</h2>
-    <h4> 나이 : {age}</h4>
-    </div>
-  );
-}
-
-
-// proptypes 통해 이름이 다른지, 요소가 빠졌는지 형식이 다른지 체크할수 있다.
-// isRequired는 있어야 하는 요소
-// 자세한 내용은 React Doc에서 propTypes를 검색
-Food.propTypes = {
-  fav : PropTypes.string.isRequired,
-  age : PropTypes.number.isRequired
-};
-
-function App() {
-  return (
-    <div>
-      {whoILike.map(who => 
-      <Food  
-      key = {who.key} 
-      fav = {who.name } 
-      age = {who.value}/>)}
-    </div>
-  );
+class App extends React.Component{
+  state = {
+    count: 0
+  };
+  add = () => {
+    console.log("add");
+  };
+  minus = () => {
+    console.log("minus");
+  };
+  render() {
+    return (
+      <div>
+        <h1>The number is: {this.state.count}</h1>
+        <button onClick={this.add}>Add</button>
+        <button onClick={this.minus}>Minus</button>
+      </div>
+    );
+  }
 }
 
 export default App;
