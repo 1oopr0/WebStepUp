e32f8efa21c98fa71cf551c6f2d6c8131c29fb5e
1oopr0 <1oo.ooooo.pro@gmail.com>
2021-07-25 18:24:04 +0900

    className and map index

--- #003 My React Movie App/my-react-movie-app/src/App.js
+++ #003 My React Movie App/my-react-movie-app/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import axios from "axios";
 import Movie from "./movie";
+import "./App.css";
 
 class App extends React.Component{
   state = {
@@ -21,15 +22,34 @@ class App extends React.Component{
     this.getMovies();
   }
   
+  // java script의 class 와 React Component의 class간 혼동에 의해 className을 사용함
   render() {
     const { isLoading, movies } = this.state;
     return (
+      <section className="container">
       <div>
-        {isLoading ? "Loading": movies.map(movie => {
-          console.log(movie);
-          return <Movie key={movie.id} id={movie.id} year={movie.year} title={movie.title} summary={movie.summary} poster={movie.medium_cover_image} />
-        })}
+        {isLoading 
+          ?(
+            <div className="loader">
+              <span className="loader__test">Loading...</span>
+            </div>
+          )
+          : (
+            <div className="movies">
+              {movies.map(movie => 
+             <Movie 
+              key={movie.id} 
+              id={movie.id} 
+              year={movie.year} 
+              title={movie.title} 
+              summary={movie.summary} 
+              poster={movie.medium_cover_image}
+              genres={movie.genres}/>)}
+            </div>
+          )
+        }
       </div>
+      </section>
     );
   }
 }
