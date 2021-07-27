2aceec3278d30f5c426beb8c9d293d9ffe57cbec
1oopr0 <1oo.ooooo.pro@gmail.com>
2021-07-27 20:01:13 +0900

    Updates

--- static/js/main.5bba1f01.chunk.js
+++ static/js/main.5bba1f01.chunk.js
@@ -1,2 +1,2 @@
 (this["webpackJsonpmy-react-movie-app"]=this["webpackJsonpmy-react-movie-app"]||[]).push([[0],{27:function(e,t,n){"use strict";n.r(t);var c=n(0),a=n.n(c),r=n(14),o=n.n(r),i=n(15),s=n(1),p=n(6);var u=function(){return Object(p.jsx)("span",{children:"About  this page: I built it because I love movies."})};var b=function(){return Object(p.jsx)(i.a,{children:Object(p.jsx)(s.a,{path:"/about",component:u})})};o.a.render(Object(p.jsx)(a.a.StrictMode,{children:Object(p.jsx)(b,{})}),document.getElementById("root"))}},[[27,1,2]]]);
-//# sourceMappingURL=main.e75c8e1a.chunk.js.map
+//# sourceMappingURL=main.5bba1f01.chunk.js.map
