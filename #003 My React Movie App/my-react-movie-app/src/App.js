ddcb4524bc0facdc425ad8e383c8956d50e9dcd8
1oopr0 <1oo.ooooo.pro@gmail.com>
2021-07-28 23:10:05 +0900

    Redirecting

--- #003 My React Movie App/my-react-movie-app/src/App.js
+++ #003 My React Movie App/my-react-movie-app/src/App.js
@@ -3,16 +3,16 @@ import {HashRouter, Route} from "react-router-dom";
 import Home from "./routes/Home";
 import About from "./routes/About";
 import Navigation from './components/Navigation';
+import Detail from "./routes/Detail";
 
 function App() {
-    return 
-    {/* BrowserRouter는 주소에 #이 없지만, gh-page 설정이 복잡하다.*/}
-    <HashRouter>
+    /* BrowserRouter는 주소에 #이 없지만, gh-page 설정이 복잡하다.*/
+    return <HashRouter>
         {/* Router 안에 있어야, Navigation 의 Link가 작용한다.*/}
-        <Navigation />
-        {/* 한개만 렌더하기 exact */}
+        <Navigation/> {/* 한개만 렌더하기 exact */}
         <Route path="/" exact={true} component={Home}/>
         <Route path="/about" component={About}/>
+        <Route path="/movie/:id" component={Detail}/>
     </HashRouter>
 }
 
