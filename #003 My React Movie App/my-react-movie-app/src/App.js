0dd16432fc57b1ab00e64e15dbe75e3c73d5983d
1oopr0 <1oo.ooooo.pro@gmail.com>
2021-07-28 21:13:20 +0900

    Link and Browser Router

--- #003 My React Movie App/my-react-movie-app/src/App.js
+++ #003 My React Movie App/my-react-movie-app/src/App.js
@@ -2,9 +2,14 @@ import React from 'react';
 import {HashRouter, Route} from "react-router-dom";
 import Home from "./routes/Home";
 import About from "./routes/About";
+import Navigation from './components/Navigation';
 
 function App() {
-    return <HashRouter>
+    return 
+    {/* BrowserRouter는 주소에 #이 없지만, gh-page 설정이 복잡하다.*/}
+    <HashRouter>
+        {/* Router 안에 있어야, Navigation 의 Link가 작용한다.*/}
+        <Navigation />
         {/* 한개만 렌더하기 exact */}
         <Route path="/" exact={true} component={Home}/>
         <Route path="/about" component={About}/>
